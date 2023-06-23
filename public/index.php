77bc9690e027f8668522814029b80607cc3776cd
dmitriy30011997 <112504672+dmitriy30011997@users.noreply.github.com>
2023-06-23 15:50:00 +0300

    final commit

--- public/index.php
+++ public/index.php
@@ -12,6 +12,6 @@
         <app></app> <!-- Vue.js компонент -->
     </div>
 
-    <script src="{{ asset('js/app.js') }}"></script> 
+    <script src="{{ asset('resources/js/app.js') }}"></script> 
 </body>
 </html>
---
+++ resources/js/components/app.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <h1>My Pizza Delivery App</h1>
+      <PizzaList />
+    </div>
+  </template>
+  
+  <script>
+  import PizzaList from '@/components/PizzaList.vue';
+  
+  export default {
+    name: 'App',
+    components: {
+      PizzaList,
+    },
+  };
+  </script>
+  
+  <style>
+  /* Стили для App.vue */
+  </style>
+  
